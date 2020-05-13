2413cb8d818d346f202666a21a8e650b552788ed
Chernogorsky Fedor <fyodorchernogor@gmail.com>
2020-05-13 14:48:33 +0300

    Change build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if pip3 install -r requirements.txt
+if pip3 install -r requirements.txt && sudo apt install antlr4 && cd src/antlr_parser && antlr4 -Dlanguage=Python3 Gram.g4
 then
 echo -e "Complete"
 else
